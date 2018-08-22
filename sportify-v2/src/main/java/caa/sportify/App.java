829b83733c515c9498eb6de0ecc87896be90dab7
Crispin A. <Crispinakomea@gmail.com>
2018-08-22 23:51:02 +0100

    Fixtures update on start commented out. Team update required.

--- sportify-v2/src/main/java/caa/sportify/App.java
+++ sportify-v2/src/main/java/caa/sportify/App.java
@@ -5,7 +5,6 @@ import java.io.IOException;
 import java.sql.SQLException;
 import java.text.ParseException;
 
-import caa.sportify.database.Fixtures;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -18,7 +17,7 @@ import javafx.stage.Stage;
 public class App extends Application {
 
 	public static void main(String[] args) throws SQLException, FileNotFoundException, IOException, ParseException {
-		Fixtures.startUpUpdate();
+		// Fixtures.startUpUpdate();
 		launch(args);
 	}
 
@@ -38,7 +37,7 @@ public class App extends Application {
 	 */
 	private Stage getMainStage() throws IOException {
 		Stage stage = new Stage();
-		stage.setTitle(com.vendor.App.getName());
+		stage.setTitle(Config.getName());
 		stage.setMinWidth(1250.0);
 		stage.setMinHeight(700.0);
 		stage.setScene(getMainScene());
